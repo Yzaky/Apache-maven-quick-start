4b35c52418ec6710120b827e0dd812d37d247b50
Youssef Zaky <youssefzaky94@hotmail.com>
2017-06-20 01:55:44 -0400

    Editted POM

---
+++ src/main/java/Youssef/programming/training/Application.java
@@ -0,0 +1,21 @@
+package Youssef.programming.training;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+		
+    }
+
+   
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+		Application app = new Application();
+
+    }
+}
